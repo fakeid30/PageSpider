86dde238a666081eabc85698c97b05435f279428
Chorompotro <junayeed@protonmail.com>
2020-03-23 00:03:49 +0600

    added database

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,12 +1,18 @@
+import sqlite3 as lite
+
+
 def create_database(database_path: str):
-
-
-# TODO: generate the databass
-pass
-
-
-def save_words_to_database(database_path: str, words_list: list):
-
-
-# TODO: save th words to the database
-pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("drop table if exists words")
+        ddl = "create table words(word text not null constraint table_name_pk primary key, usage_count int default 1 not null);create unique index table_name_word_uindexon words (word);"
+        cur.execute(ddl)
+        ddl = "create unique INDEX words_word_uindex ON words(word)"
+        cur.execute(ddl)
+    conn.close()
+# def save_words_to_database(database_path: str, words_list: list):
+#
+#
+# # TODO: save th words to the database
+# pass
