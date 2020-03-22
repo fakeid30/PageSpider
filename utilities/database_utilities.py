07bd35e7970589ab547a893fb465d36ea6ba4d74
Chorompotro <junayeed@protonmail.com>
2020-03-22 21:16:09 +0600

    Stubbing out the program

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    #TODO: generate the databass
+pass
+
+def save_words_to_database(database_path:str, words_list: list):
+    #TODO: save th words to the database
+pass
+
