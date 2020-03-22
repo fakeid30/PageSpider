4ac74881e7637216081b878e24464c82252b5eb9
Chorompotro <junayeed@protonmail.com>
2020-03-22 21:54:17 +0600

    Linter fixes

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,8 +1,12 @@
 def create_database(database_path: str):
-    #TODO: generate the databass
+
+
+# TODO: generate the databass
 pass
 
-def save_words_to_database(database_path:str, words_list: list):
-    #TODO: save th words to the database
-pass
 
+def save_words_to_database(database_path: str, words_list: list):
+
+
+# TODO: save th words to the database
+pass
